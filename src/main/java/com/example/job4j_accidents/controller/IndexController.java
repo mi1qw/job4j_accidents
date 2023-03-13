2c1d4c939b862d3a1ca110612719884f0b43a281
mi1qw <123mib@gmail.com>
2023-03-13 22:54:45 +0200

    IndexController

--- src/main/java/com/example/job4j_accidents/controller/IndexController.java
+++ src/main/java/com/example/job4j_accidents/controller/IndexController.java
@@ -2,15 +2,14 @@ package com.example.job4j_accidents.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class IndexController {
-  /*
-   @GetMapping("/")
+    @GetMapping("/")
     public String index() {
         log.info("index");
         return "index";
     }
-   */
 }
