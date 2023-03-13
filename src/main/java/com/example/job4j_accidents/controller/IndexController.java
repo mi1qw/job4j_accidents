257faed8a48e58da0460660846a578166c1b074e
mi1qw <123mib@gmail.com>
2023-03-13 23:15:37 +0200

    navbar with name

--- src/main/java/com/example/job4j_accidents/controller/IndexController.java
+++ src/main/java/com/example/job4j_accidents/controller/IndexController.java
@@ -2,14 +2,15 @@ package com.example.job4j_accidents.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 @Slf4j
 public class IndexController {
     @GetMapping("/")
-    public String index() {
-        log.info("index");
+    public String index(final Model model) {
+        model.addAttribute("user", "Petr Arsentev");
         return "index";
     }
 }
