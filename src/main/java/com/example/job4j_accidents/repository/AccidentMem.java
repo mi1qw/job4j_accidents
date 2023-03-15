94e29e512f998b096a831daffb9ae01203e6a0ff
mi1qw <123mib@gmail.com>
2023-03-15 15:13:47 +0200

    init AccidentMem

--- src/main/java/com/example/job4j_accidents/repository/AccidentMem.java
+++ src/main/java/com/example/job4j_accidents/repository/AccidentMem.java
@@ -3,7 +3,6 @@ package com.example.job4j_accidents.repository;
 import com.example.job4j_accidents.model.Accident;
 import com.example.job4j_accidents.model.AccidentType;
 import com.example.job4j_accidents.model.Rule;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -14,30 +13,26 @@ import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
 @Repository
-@Slf4j
 public class AccidentMem {
     private final Map<Integer, Accident> accidents = new ConcurrentHashMap<>();
     private final AtomicInteger id = new AtomicInteger(0);
 
     {
-        int id = this.id.getAndIncrement();
-        accidents.put(id, new Accident(id, "Нурушение парковки",
+        create(new Accident(0, "Нурушение парковки",
                 "Парковка транспортных средств на местах, "
                         + "предназначенных для бесплатной парковки "
                         + "транспортных средств, лицами, которые не имеют"
                         + " соответствующих льгот", "ул.Васина д1",
                 new AccidentType(2, "Машина и человек"),
                 Set.of(new Rule(1, "Статья. 1"))));
-        id = this.id.getAndIncrement();
-        accidents.put(id, new Accident(id, "Нурушение парковки",
+        create(new Accident(0, "Нурушение парковки",
                 "Парковка транспортных средств на местах,  "
                         + "предназначенных для бесплатной парковки "
                         + "транспортных средств, лицами, которые не имеют"
                         + " соответствующих льгот", "ул.Анны д1",
                 new AccidentType(2, "Машина и человек"),
                 Set.of(new Rule(1, "Статья. 1"))));
-        id = this.id.getAndIncrement();
-        accidents.put(id, new Accident(id, "Нарушение перезда перекрёстка",
+        create(new Accident(0, "Нарушение переезда перекрёстка",
                 "Не уступил дорогу транспортным средствам, приближающимся по "
                         + "главной дороге",
                 "ул.Петра д1",
