ee633909bc0aac1a21a712b54267812b11785e4a
mi1qw <123mib@gmail.com>
2023-03-20 05:19:44 +0200

    JDBC Authentication

--- src/main/java/com/example/job4j_accidents/config/SecurityConfig.java
+++ src/main/java/com/example/job4j_accidents/config/SecurityConfig.java
@@ -1,20 +1,25 @@
 package com.example.job4j_accidents.config;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
+import org.springframework.security.provisioning.UserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+
+import javax.sql.DataSource;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
 @Configuration
 @EnableWebSecurity
 public class SecurityConfig {
+    @Autowired
+    private DataSource ds;
 
     @Bean
     public SecurityFilterChain filterChain(final HttpSecurity http) throws Exception {
@@ -47,19 +52,13 @@ public class SecurityConfig {
     }
 
     @Bean
-    public InMemoryUserDetailsManager userDetailsService(final PasswordEncoder passwordEncoder) {
-        return new InMemoryUserDetailsManager(
-                User.builder()
-                        .username("admin")
-                        .password(passwordEncoder.encode("123"))
-                        .roles("USER", "ADMIN")
-                        .build(),
-                User.builder()
-                        .username("user")
-                        .password(passwordEncoder.encode("123"))
-                        .roles("USER")
-                        .build()
-        );
+    public UserDetailsManager userDetailsService() {
+        JdbcUserDetailsManager manager = new JdbcUserDetailsManager(ds);
+        manager.setUsersByUsernameQuery(
+                "select username, password, enabled from users where username = ?");
+        manager.setAuthoritiesByUsernameQuery(
+                "select username, authority from authorities where username = ?");
+        return manager;
     }
 
     @Bean
--- src/main/java/com/example/job4j_accidents/controller/IndexController.java
+++ src/main/java/com/example/job4j_accidents/controller/IndexController.java
@@ -3,6 +3,8 @@ package com.example.job4j_accidents.controller;
 import com.example.job4j_accidents.service.AccidentService;
 import lombok.AllArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -14,7 +16,9 @@ public class IndexController {
     private final AccidentService accidentService;
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("user", "Petr Arsentev");
+        User principal = (User) SecurityContextHolder.getContext()
+                .getAuthentication().getPrincipal();
+        model.addAttribute("user", principal.getUsername());
         model.addAttribute("accidents", accidentService.allAccidents());
         return "index";
     }
